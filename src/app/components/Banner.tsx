05ccb2a29ce7aa9f9b7401f56d35b8f98b6a46db
jo <jyothishkpk@gmail.com>
2025-06-18 12:15:26 +0530

    iii

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
 
