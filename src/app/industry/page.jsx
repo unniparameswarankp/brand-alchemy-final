528c35c1c32f76f633aacb82d0a5266d74865921
jo <jyothishkpk@gmail.com>
2025-06-19 13:47:16 +0530

    ttt

---
+++ src/app/industry/page.jsx
@@ -0,0 +1,16 @@
+'use client';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import IndustryHoverSection from '../components/IndustryHoverSection';
+import IndustryBanner from '../components/IndustryBanner';
+
+export default function IndustryPage() {
+  return (
+    <>
+      <Header />
+      <IndustryBanner />
+      <IndustryHoverSection />
+      <Footer />
+    </>
+  );
+}
