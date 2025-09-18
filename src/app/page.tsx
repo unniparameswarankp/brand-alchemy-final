d3755f347844d18c475b53f2911e937c7a788fd3
jo <jyothishkpk@gmail.com>
2025-09-18 16:18:25 +0530

    opop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import ScrollSteps from "./components/ScrollSteps";
 import CaseStudiesScroll from "./components/CasestudiesScroll";
 import OurStory from "./components/OurStory";
 import Footer from "./components/Footer";
+import ScrollVideo from "./components/ScrollVideo";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
 <ScrollSteps />
 <CaseStudiesScroll />
 <OurStory />
+<ScrollVideo />
 <Footer />
    </>
   );
